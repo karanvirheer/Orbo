6ecace977da33e6c2c14f0d186b54b7d8375106b
Karanvir Heer <karanvirheer@live.com>
2019-01-20 18:33:54 -0500

    Updated commenting

--- Scripts/CameraMovement.cs
+++ Scripts/CameraMovement.cs
@@ -7,7 +7,9 @@ public class CameraMovement : MonoBehaviour
 
     public float speed;
 
-    // Update is called once per frame
+    /* Update is called once per frame
+     * Rotates the camera at a set speed around the y axis.
+     */
     void Update()
     {
         transform.Rotate(0, speed * Time.deltaTime, 0);
